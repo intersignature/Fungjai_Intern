43c9c5d7e5133f4e7fce7c232e59a434f4fbce74
intersignature <intersignature@hotmail.com>
2018-03-21 03:45:53 +0700

    Add Model
    - Create model package for contain model class.
    - Add DataModel
    - Add Retrofit to project

---
+++ app/src/main/java/intern/fungjai/sirichai/fungjai_intern/model/DataModel.java
@@ -0,0 +1,87 @@
+
+package intern.fungjai.sirichai.fungjai_intern.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class DataModel {
+
+    @SerializedName("artist")
+    @Expose
+    private String artist;
+    @SerializedName("cover")
+    @Expose
+    private String cover;
+    @SerializedName("song")
+    @Expose
+    private String song;
+    @SerializedName("type")
+    @Expose
+    private String type;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("url")
+    @Expose
+    private String url;
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getCover() {
+        return cover;
+    }
+
+    public void setCover(String cover) {
+        this.cover = cover;
+    }
+
+    public String getSong() {
+        return song;
+    }
+
+    public void setSong(String song) {
+        this.song = song;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+}
